7b178f287c0091b719919b0962efede214f36777
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-03-09 16:32:10 -0300

    feat: implementing handlers

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
 	log.Println("[INFO] Server running on port: 6379")
 
-	l, err := net.Listen("tcp", "6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		log.Fatalln("[ERROR] Cannot acess port 6379:", err)
 	}
@@ -26,15 +27,35 @@ func main() {
 		resp := NewResp(conn)
 		value, err := resp.Read()
 		if err != nil {
-			if errors.Is(io.EOF, err) {
+			if errors.Is(err, io.EOF) {
 				break
 			}
-			log.Fatalln("[ERROR] something went wrong reading the client:", err)
+			log.Fatalln("[ERROR] Something went wrong reading the client:", err)
 		}
 
-		_ = value
+		if value.typ != "array" {
+			log.Println("[WARN] Invalid request, expected array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			log.Println("[WARN] Invalid request, expected array size > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
 
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		handler, ok := Handlers[command]
+		if !ok {
+			log.Println("[WARN] Invalid command: ", command)
+			_ = writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+		_ = writer.Write(result)
 	}
 }
