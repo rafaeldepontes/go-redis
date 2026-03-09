5d9f125b10e6f15e2ef01ac94459ba9403fdb579
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-03-09 17:00:47 -0300

    feat: adding the set command

--- handler.go
+++ handler.go
@@ -1,5 +1,10 @@
 package main
 
+import "sync"
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 }
@@ -9,4 +14,19 @@ func ping(args []Value) Value {
 		return Value{typ: "string", str: "PONG"}
 	}
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
 }
